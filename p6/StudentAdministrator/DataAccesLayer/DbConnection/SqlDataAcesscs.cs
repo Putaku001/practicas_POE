5b27cfeb84301e787b8a9157f8b9843b56463d7a
Putaku001 <kennethgranados64@gmail.com>
2024-11-06 20:23:27 -0600

    elimine las practicas antifuas por error xD

---
+++ p6/StudentAdministrator/DataAccesLayer/DbConnection/SqlDataAcesscs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.DbConnection
+{
+    public class SqlDataAcesscs
+    {
+        private readonly string _connectionString;
+        public SqlDataAcesscs()
+        {
+            _connectionString = "Data Source=ALEJANDRO\\SQLEXPRESS;Initial Catalog=StudentAdministratorDB;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection( _connectionString );
+        }
+    }
+}
---
+++ p6/StudentAdministrator/DataAccesLayer/Repositories/CareerRepository.cs
@@ -0,0 +1,53 @@
+﻿using DataAccesLayer.DbConnection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLayer.Entities;
+
+namespace DataAccesLayer.Repositories
+{
+    public class CareerRepository
+    {
+        private SqlDataAcesscs _dbConnection;
+
+        public CareerRepository()
+        {
+            _dbConnection = new SqlDataAcesscs();
+        }
+
+        public DataTable GetCareers()
+        {
+            DataTable careersTable = new DataTable();
+            using (var connection = _dbConnection.GetConnection())
+            {
+                string query = "SELECT * FROM career";
+                SqlCommand command = new SqlCommand (query, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                careersTable.Load(reader);
+            }
+
+            return careersTable;
+        }
+
+        public void AddCareer(Career career)
+        {
+            using (var connection = _dbConnection.GetConnection())
+            {
+                string query = "INSERT INTO career VALUES(@NameCareer, @DescriptionCareer)";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@NameCareer", career.nameCareer);
+                command.Parameters.AddWithValue("@DescriptionsCareer", career.nameCareer);
+
+                command.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
