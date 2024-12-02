fac04dacb59a1311ac60268f40b5ed16699640a7
Putaku001 <kennethgranados64@gmail.com>
2024-12-01 20:17:03 -0600

    practica 16

---
+++ p16/StudentAdministrator/DataAccessLayer/Repositories/CareerRepository.cs
@@ -0,0 +1,103 @@
+﻿using CommonLayer.Entities;
+using DataAccessLayer.DbConnection;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+	public class CareerRepository
+	{
+		private SqlDataAccess _dbConnection;
+
+        public CareerRepository()
+        {
+            _dbConnection = new SqlDataAccess();
+        }
+
+        public DataTable GetCareers(string searchCareer)
+        {
+			DataTable careersTable = new DataTable();
+
+			using (var connection = _dbConnection.GetConnection())
+			{
+                // Nombre del procedimiento almacenado
+                string storedProcedureName = "sp_SelectCareer";
+
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@SearchCareer", '%' + searchCareer + '%');
+				connection.Open();
+
+				SqlDataReader reader = command.ExecuteReader();
+				careersTable.Load(reader);
+			}
+
+			return careersTable;
+		}
+
+		public void AddCareer(Career career)
+		{
+			using (var connection = _dbConnection.GetConnection())
+			{
+                // Nombre del procedimiento almacenado
+                string storedProcedureName = "sp_InsertCareer";
+
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@nameCareer", career.nameCareer);
+                command.Parameters.AddWithValue("@descriptionCareer", career.descriptionCareer);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+		}
+		public void EditCareer(Career career)
+		{
+			using (var connection = _dbConnection.GetConnection())
+			{
+                // Nombre del procedimiento almacenado
+                string storedProcedureName = "sp_UpdateCareer";
+
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@nameCareer", career.nameCareer);
+				command.Parameters.AddWithValue("@descriptionCareer", career.descriptionCareer);
+				command.Parameters.AddWithValue("@idCareer", career.idCareer);
+				connection.Open();
+
+				command.ExecuteNonQuery();
+			}
+		}
+
+		public void DeleteCareer(int id)
+		{
+			using (var connection = _dbConnection.GetConnection())
+			{
+                // Nombre del procedimiento almacenado
+                string storedProcedureName = "sp_DeleteCareer";
+
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@idCareer", id);
+				connection.Open();
+
+				command.ExecuteNonQuery();
+			}
+		}
+
+	}
+}
---
+++ p16/StudentAdministrator/DataAccessLayer/Repositories/StudentRepository.cs
@@ -0,0 +1,105 @@
+﻿using CommonLayer.Entities;
+using DataAccessLayer.DbConnection;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class StudentRepository
+    {
+        private SqlDataAccess _dbConnection;
+
+        public StudentRepository()
+        {
+            _dbConnection = new SqlDataAccess();
+        }
+
+        public DataTable GetStudents(string search)
+        {
+            DataTable studentTable = new DataTable();
+
+            using (var connection = _dbConnection.GetConnection())
+            {
+                // Nombre del procedimiento almacenado
+                string storedProcedureName = "sp_SelectStudent";
+
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Search", '%' + search + '%');
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                studentTable.Load(reader);
+            }
+
+            return studentTable;
+        }
+
+        public void AddStudent(Student student)
+        {
+            using (var connection = _dbConnection.GetConnection())
+            {
+                // Nombre del procedimiento almacenado
+                string storedProcedureName = "sp_InsertStudent";
+
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@nameStudent", student.nameStudent);
+                command.Parameters.AddWithValue("@lastnameStudent", student.lastnameStudent);
+                command.Parameters.AddWithValue("@idCareerStudent", student.idCareerStudent);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+        public void EditStudent(Student student)
+        {
+            using (var connection = _dbConnection.GetConnection())
+            {
+                // Nombre del procedimiento almacenado
+                string storedProcedureName = "sp_UpdateStudent";
+
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@nameStudent", student.nameStudent);
+                command.Parameters.AddWithValue("@lastnameStudent", student.lastnameStudent);
+                command.Parameters.AddWithValue("@idCareerStudent", student.idCareerStudent);
+                command.Parameters.AddWithValue("@idStudent", student.idStudent);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteStudent(int id)
+        {
+            using (var connection = _dbConnection.GetConnection())
+            {
+                // Nombre del procedimiento almacenado
+                string storedProcedureName = "sp_DeleteStudent";
+
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@idStudent", id);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+    }
+}
